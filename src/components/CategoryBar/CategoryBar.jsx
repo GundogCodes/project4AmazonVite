2044684d1cacbfd8e8eec614c2e8615d9e84b8fb
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-09-12 16:57:08 -0400

    i think this makes more sense from the catbar

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -3,39 +3,55 @@ import { slide as Menu } from 'react-burger-menu'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function CategoryBar({ categories, activeCategory, setActiveCategory }){
+export default function CategoryBar({ categories, searchableItems }){
+  //use states
   const [category, setCategory] = useState('')
+  const navigate = useNavigate()
 
-  // .find req.params by category # and send to CategoryPage
-  // set active category to category # and send to CategoryPage
 
+  //trying to catch the corresponding category
+  function handleOnClick(e){
+    //defining variables
+    let clickedCategory = e.target.innerText
+    clickedCategory.toLowerCase()
+    category(clickedCategory)
+      
+    //sending the category to the category page
+      setCategoryItems(itemsArr)
+      navigate('/categories', {
+        state: {
+          category: category
+        }
+      })
+    }
+  }
 
     return(
       <>
         <Menu>
-          <a className='menu-item' href='/categories'>
-          {/* send category num */}
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Electronics
           </a>
-          <a className='menu-item' href='/categories'>
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Toys
           </a>
-          <a className='menu-item' href='/categories'>
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Gym Equipment
           </a>
-          <a className='menu-item' href='/categories'>
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Air
           </a>
-          <a className='menu-item' href='/categories'>
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Clothes
           </a>
-          <a className='menu-item' href='/categories'>
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Cosmetics
           </a>
-          <a className='menu-item' href='/categories'>
+          <a className='menu-item' href='/categories' onClick={handleOnClick}>
             Books
           </a>
         </Menu>
       </>
     )
+
 }
--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,88 +1,35 @@
-import { useState, useEffect, useRef } from 'react'
-import * as itemsAPI from '../../utilities/items-api.cjs'
-import * as ordersAPI from '../../utilities/orders-api.cjs'
-import styles from './CategoryPage.module.scss'
-import MenuList from '../../components/MenuList/MenuList.jsx'
-import CategoryBar from '../../components/CategoryBar/CategoryBar.jsx'
+import CategoryBar from '../CategoryBar/CategoryBar'
 
-export default function Shop({}) {
-    //defining useStates
-    const [menuItems, setMenuItems] = useState([])
-    const [activeCategory, setActiveCategory] = useState('')
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { getById } from '../../utilities/items-api.cjs';
+import styles from './ItemPage.module.scss';
+import { Row, Col, Image, ListGroup, Card } from 'react-bootstrap';
+import Ratings from '../../components/Ratings/Ratings';
+import { addToCart, getCart } from '../../utilities/orders-api.cjs';
+import { getWishList, addToWishList } from '../../utilities/wishList-api.cjs';
 
-    const categoryRef = useRef()
-    //defining useEffects
-    useEffect(() => {
-        async function getItems() {
-            const items = await itemsAPI.getAll() //does the get all items
-            categoryRef.current = items.reduce((categories, item) => {
-                const category = item.category.name
-                //if the category is already in the array, return the array
-                return categories.includes(category) ? categories : [...categories, category] //if the item is not in the array of the category, add it
-            }, [])
-            setMenuItems(items)
-            setActiveCategory(categoryRef.current[0])
-        }
-        //calls getItems function in the useEffect
-        getItems()
-        async function getCart() {
-            const cart = await ordersAPI.getCart()
-            setCart(cart)
-        }
-        getCart()
-    }, [])
+export default function CategoryPage({searchableItems, user, setUser}){
+//handle click of item chosen from user, using function from searchbar
+    function handleLiClick(e){
+        const clickedItem = e.target.innerText
+        console.log('clickedItem ', clickedItem)
+        const indexOfClickedItem = itemNameArr.indexOf(clickedItem)
+        const idOfClickedItem = itemIdArr[indexOfClickedItem]
+        console.log('indexofclickedItem', indexOfClickedItem)
+        console.log('idOfClickedItem',idOfClickedItem)
+        //navigate(`item/${idOfClickedItem}`)
+        //setClickedItemID(idOfClickedItem)
 
-    //event handlers
-    async function handleAddToCart(itemId) {
-        const cart = await ordersAPI.addToCart(itemId)
-        setCart(cart)
+        console.log('ClICKEDITEM ID : ', idOfClickedItem)
+        navigate(`/item/${idOfClickedItem}`)
+
     }
 
-    return (
-        <main className={styles.MainContainer}>
-            <div>
-                <CategoryBar
-                    categories={categoryRef.current}
-                    activeCategory={activeCategory}
-                    setActiveCategory={setActiveCategory}
-                />
-            </div>
-            <MenuList
-                menuItems={activeCategory === 'All' ? menuItems : menuItems.filter(item => item.category.name === activeCategory)}
-                handleAddToCart={handleAddToCart}
-            />
-        </main>
+
+    return(
+        <div>
+
+        </div>
     )
 }
-
-// export default function CategoryPage({ searchableItems }){
-
-// //defining useStates
-// const [category, setCategory] = useState('')
-// const [categoryItemsArr, setCategoryItems] = useState('')
-
-// // capturing category name from CategoryBar component
-
-
-// //looking for specific category in searchableItems
-// const categoryItems = searchableItems.filter
-//     (item => item.category === category)
-// categoryItemsArr.push(categoryItems)
-
-//     return(
-//         <main className={styles.MainContainer}>
-//             <div className={styles.CategoryContainer}>
-//                 <div className={styles.CategoryTitle}>
-//                     <h1>Category</h1>
-//                 </div>
-//                 <div className={styles.CategoryItems}>
-//                     <div className={styles.CategoryItem}>
-//                     {/* display all captured items here */}
-//                         <h2>Item</h2>
-//                     </div>
-//                 </div>
-//             </div>
-//         <Footer />
-//         </main>
-//     )
-// }
