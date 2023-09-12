ab5ae58a74ce8bb3de7a78a0d862d083b504b68d
AndrewELe <125837056+AndrewELe@users.noreply.github.com>
2023-09-12 18:13:11 -0400

    working on catpage again

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -3,13 +3,20 @@ import CategoryBar from '../CategoryBar/CategoryBar'
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getById } from '../../utilities/items-api.cjs';
-import styles from './ItemPage.module.scss';
-import { Row, Col, Image, ListGroup, Card } from 'react-bootstrap';
-import Ratings from '../../components/Ratings/Ratings';
-import { addToCart, getCart } from '../../utilities/orders-api.cjs';
-import { getWishList, addToWishList } from '../../utilities/wishList-api.cjs';
+import styles from './CategoryPage.module.scss';
 
-export default function CategoryPage({searchableItems, user, setUser}){
+export default function CategoryPage({searchableItems, category}){
+//arrays and variables
+const { id } = useParams().id
+const itemsArr = []
+
+//capture the items of the category and set array of all the items in the cateogry from categoryBar
+    for(let item of searchableItems){
+        if(item.category === category){
+            itemsArr.push(item)
+        }
+    }
+
 //handle click of item chosen from user, using function from searchbar
     function handleLiClick(e){
         const clickedItem = e.target.innerText
@@ -23,13 +30,33 @@ export default function CategoryPage({searchableItems, user, setUser}){
 
         console.log('ClICKEDITEM ID : ', idOfClickedItem)
         navigate(`/item/${idOfClickedItem}`)
-
     }
 
 
-    return(
-        <div>
+//catching item id
+    useEffect(() => {
+        (async () => {
+        try {
+            const item = await getById(id);
+            setItem(item);
+        } catch (error) {
+            console.log(error);
+        }
+        })();
+    }, [id]);
 
+    return(
+        <div className={styles.rowContainer}>
+            <div className={`${styles.imageContainer} flex-ctr-ctr`}>
+                <img src={item.img} className={styles.itemImg} />
+            </div>
+            <div className={styles.itemInfo}>
+                <div className={styles.name}>{item.name}</div>
+                <div className={styles.buy}>
+                    <span>just buy me</span>
+                    <button className='btn-sm' href='/items'/>
+                </div>
+            </div>    
         </div>
     )
 }
