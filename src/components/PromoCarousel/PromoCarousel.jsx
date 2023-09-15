221d7879359bf2fabda091682679dd7ab85f0531
TJCole03 <tjc6254@gmail.com>
2023-09-14 21:05:17 -0400

    merging with dev

--- src/components/PromoCarousel/PromoCarousel.jsx
+++ src/components/PromoCarousel/PromoCarousel.jsx
@@ -27,38 +27,22 @@ export default function PromoCarousel({promotionItems}) {
   return (
    <div className={styles.container}>
     <Carousel activeIndex={styles.index} onSelect={handleSelect} className={styles.Carousel}>
-<<<<<<< HEAD
-      <Carousel.Item className={styles.CarouselItem} >
-      <Link to={`item/${promotionItems[0][53]._id}`} > <Image 
-=======
 
       <Carousel.Item className={styles.CarouselItem} styles={{backgroundPosition:'cover'}} >
       <Link to={`item/${promotionItems[0][1]._id}`} > <Image 
->>>>>>> dev
           className={styles.img}
           src='./img/sonos.png'
           /> </Link>
-<<<<<<< HEAD
-        <Carousel.Caption className={styles.caption}>
-          <h3 className={styles.cardTitle}>{carouselItems[0][53].name}</h3>
-          <p className={styles.itemDesc}>{promotionItems[0][53].description}</p>
-          <a href= {`item/${promotionItems[0][53]._id}`} className={styles.sm} > Buy Now! </a>
-=======
         <Carousel.Caption style={{color:'black'}} className={styles.caption}>
           <h3 className={styles.cardTitle}>{carouselItems[0][1].name}</h3>
           <p className={styles.itemDesc}>{promotionItems[0][1].description}</p>
           <a href= {`item/${promotionItems[4]._id}`} className={styles.sm} > Buy Now! </a>
->>>>>>> dev
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className={styles.CarouselItem} >
       <Link to={`item/${promotionItems[0][50]._id}`} > <Image 
           className={styles.img}
-<<<<<<< HEAD
-          src='./img/gnome.png'
-=======
           src='./img/lego-barbie.png'
->>>>>>> dev
           /> </Link>
         <Carousel.Caption className={styles.caption}>
           <h3 className={styles.cardTitle}>{carouselItems[0][50].name}</h3>
@@ -67,24 +51,14 @@ export default function PromoCarousel({promotionItems}) {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className={styles.CarouselItem} >
-<<<<<<< HEAD
-      <Link to={`item/${promotionItems[0][36]._id}`} > <Image 
-=======
       <Link to={`item/${promotionItems[0][27]._id}`} > <Image 
->>>>>>> dev
           className={styles.img}
           src='./img/MobyClick.png'
           /> </Link>
         <Carousel.Caption className={styles.caption}>
-<<<<<<< HEAD
-          <h3 className={styles.cardTitle}>{carouselItems[0][36].name}</h3>
-          <p className={styles.itemDesc}>{promotionItems[0][36].description}</p>
-          <a href= {`item/${promotionItems[0][36]._id}`} className={styles.sm} > Buy Now! </a>
-=======
           <h3 className={styles.cardTitle}>{carouselItems[0][27].name}</h3>
           <p className={styles.itemDesc}>{promotionItems[0][27].description}</p>
           <a href= {`item/${promotionItems[0][48]._id}`} className={styles.sm} > Buy Now! </a>
->>>>>>> dev
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className={styles.CarouselItem} >
